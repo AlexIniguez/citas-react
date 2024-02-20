fc2f58bbe0827ea431db1b2de9896a6c8ec81f67
Alejandro Iñiguez <ilex_happy99@hotmail.com>
2024-02-20 14:53:40 -0600

    Adding tailwind and postcss, another component added Paciente, and finishuing the desing part

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,14 @@ import ListadoPacientes from "./components/ListadoPacientes"
 
 function App() {
   return (
-    <>
+    <div className="container mx-auto mt-20">
       <Header />
+      <div className=" mt-12 md:flex">
       <Formulario />
       <ListadoPacientes />
-    </>
+      </div>
+      
+    </div>
   )
 }
 
---
+++ src/components/Paciente.jsx
@@ -0,0 +1,24 @@
+
+const Paciente = () => {
+  return (
+    <div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl">
+        <p className="font-bold mb-3 text-gray-700 uppercase">Nombre:{''}
+          <span className="font-normal normal-case m-1">Hook</span>
+        </p>
+        <p className="font-bold mb-3 text-gray-700 uppercase">Propietario:{''}
+          <span className="font-normal normal-case m-1">Alex</span>
+        </p>
+        <p className="font-bold mb-3 text-gray-700 uppercase">Email:{''}
+          <span className="font-normal normal-case m-1">correo@correo.com</span>
+        </p>
+        <p className="font-bold mb-3 text-gray-700 uppercase">Fecha:{''}
+          <span className="font-normal normal-case m-1">20 de Febrero de 2024</span>
+        </p>
+        <p className="font-bold mb-3 text-gray-700 uppercase">Síntomas:{''}
+          <span className="font-normal normal-case m-1">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut recusandae dolorum vitae quis, sunt pariatur accusantium porro laboriosam, dolore quas, esse ipsam. Dolores, nostrum! Odit blanditiis suscipit excepturi quo qui?</span>
+        </p>
+      </div>
+  )
+}
+
+export default Paciente
